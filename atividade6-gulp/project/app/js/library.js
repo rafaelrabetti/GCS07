a3c19a340b27e9b8d045982608f0ee70e2090716
Matheus Faria <matheus.sousa.faria@gmail.com>
2017-10-09 20:35:17 -0300

    Atividade 6

---
+++ atividade6-gulp/project/app/js/library.js
@@ -0,0 +1,5 @@
+function luck_click() {
+    var element = document.getElementById("welcomed");
+    element.innerHTML = "Play on loto, your numbers are: 4 8 15 16 23 42";
+}
+
---
+++ atividade6-gulp/project/app/js/main.js
@@ -0,0 +1,4 @@
+function clicked_on_me() {
+    var element = document.getElementById("welcomed");
+    element.innerHTML = "It works!";
+}
